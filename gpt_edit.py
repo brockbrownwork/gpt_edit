b8d2847b8adf970e948ea73cc0206e0959e846f8
Brock Brown <brockrbrown@gmail.com>
2023-03-16 08:54:53 -0400

    Add ability to display filename only in window title, and store current file path as global variable. Update save and open file functions to use the current file path and update the window title accordingly. Also, fix some formatting and spacing issues.

--- gpt_edit.py
+++ gpt_edit.py
@@ -1,10 +1,14 @@
 import tkinter as tk
 from tkinter import filedialog
+import os
 
 root = tk.Tk()
+root.title("Untitled")
 
-text_box = tk.Text(root, height=5, width=30, undo = True)
+text_box = tk.Text(root, height=5, width=30, undo=True)
 text_box.pack()
+
+current_file = None
 
 # Define a function to highlight all occurrences of the word "text"
 def highlight_text(event=None):
@@ -26,35 +30,41 @@ text_box.tag_configure("highlight", foreground="yellow", background="red")
 
 # Define a function to save the text to a file
 def save_file(event=None):
-    file_path = filedialog.asksaveasfilename(defaultextension=".txt")
-    if file_path:
-        with open(file_path, "w") as file:
+    global current_file
+    if current_file is None:
+        current_file = filedialog.asksaveasfilename(defaultextension=".txt")
+    if current_file:
+        with open(current_file, "w") as file:
             file.write(text_box.get("1.0", tk.END))
+        root.title(os.path.basename(current_file))
 
 # Bind the "ctrl+s" key combination to the save_file function
 root.bind("<Control-s>", save_file)
 
 # Define a function to save the text to a new file
 def save_as_file(event=None):
-    file_path = filedialog.asksaveasfilename(defaultextension=".txt")
-    if file_path:
-        with open(file_path, "w") as file:
+    global current_file
+    current_file = filedialog.asksaveasfilename(defaultextension=".txt")
+    if current_file:
+        with open(current_file, "w") as file:
             file.write(text_box.get("1.0", tk.END))
+        root.title(os.path.basename(current_file))
 
 # Bind the "ctrl+shift+s" key combination to the save_as_file function
 root.bind("<Control-Shift-s>", save_as_file)
 
 # Define a function to open a file
 def open_file(event=None):
-    file_path = filedialog.askopenfilename()
-    if file_path:
-        with open(file_path, "r") as file:
+    global current_file
+    current_file = filedialog.askopenfilename()
+    if current_file:
+        with open(current_file, "r") as file:
             text_box.delete("1.0", tk.END)
             text_box.insert("1.0", file.read())
+        root.title(os.path.basename(current_file))
     highlight_text()
 
 # Bind the "ctrl+o" key combination to the open_file function
 root.bind("<Control-o>", open_file)
-
 
 root.mainloop()
